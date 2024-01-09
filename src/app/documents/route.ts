acc4ab2b52fa70f4c607f23769224493cae994fb
brohithkr <brohithkr@gmail.com>
2024-01-10 04:15:06 +0530

    documents upload endpoint

---
+++ src/app/documents/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { FileParams } from 'chaincode/types';
+import * as chaincode from 'chaincode/endpoints';
+import { User } from '@/models/User';
+
+export async function POST(req: NextRequest) {
+  const fileParams: FileParams = await req.json();
+  let { contract, gateway, client } = await chaincode.getContract();
+  chaincode.createFile(contract, fileParams);
+  User.updateOne({ email: fileParams.owner }, { $push: { docs: fileParams.filename } });
+
+  return new NextResponse('Document Uploaded', { status: 200 });
+}
