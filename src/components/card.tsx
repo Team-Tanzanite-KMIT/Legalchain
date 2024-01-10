d12c78d208603e31eca3dfdcd6fe9090d9d29702
Jai-JAP <jai.jap.318@gmail.com>
2024-01-10 17:09:31 +0530

    Cardv1

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,29 +1,60 @@
 'use client';
 
-import { Card as MaterialCard, CardHeader, CardBody, Typography } from '@material-tailwind/react';
+import {
+  Card as MaterialCard,
+  CardHeader,
+  CardBody,
+  Typography,
+} from '@material-tailwind/react';
 import React from 'react';
 import { caseAttr } from '@/models/Case';
 
-// export default function Card({ children }: { children: React.ReactNode }): React.ReactNode {
-export default function Card({userCase }:
-    {userCase: caseAttr }): React.ReactNode {
-    return (
-        <MaterialCard color='gray' className='w-96'>
-            <CardBody>
-                <Typography variant="h5" color="white" className="mb-2">
-                    {userCase.caseID}
-                </Typography>
-                <Typography>
-                    {/* {cases.map((userCase) => {
-                        return (
-                            <> */}
-                            {JSON.stringify(userCase)}
-                            {/* </>
-                        )
-                    })} */}
-                </Typography>
-            </CardBody>
-
-        </MaterialCard>
-    )
+export default function Card({ userCase }: { userCase: caseAttr }): React.ReactNode {
+  return (
+    <MaterialCard color="gray" className="w-96">
+      <CardBody>
+        <Typography variant="h4" color="white" className="mb-2">
+          {userCase?.caseID}
+        </Typography>
+        <div className="flex flex-col gap-2">
+          <div className="block bg-gray-800 rounded-md p-2">
+            <Typography variant="h6" color="white" className="text-sm">
+              Judges
+            </Typography>
+            <ul className="p-3">
+              {userCase.judges.map((judge, index) => (
+                <li key={index} className="text-white">
+                  {judge}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="block bg-gray-800 rounded-md p-2">
+            <Typography variant="h6" color="white" className="text-sm">
+              Lawyers
+            </Typography>
+            <ul className="p-3">
+              {userCase.lawyers.map((lawyer, index) => (
+                <li key={index} className="text-white">
+                  {lawyer}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="block bg-gray-800 rounded-md p-2">
+            <Typography variant="h6" color="white" className="text-sm">
+              Clients
+            </Typography>
+            <ul className="p-3">
+              {userCase.clients.map((client, index) => (
+                <li key={index} className="text-white">
+                  {client}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </CardBody>
+    </MaterialCard>
+  );
 }
