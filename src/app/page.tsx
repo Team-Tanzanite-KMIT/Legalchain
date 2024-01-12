1595246c4ef68ffb6f487dd68074d4929e82363d
patelkiran185 <138752385+patelkiran185@users.noreply.github.com>
2024-01-12 13:49:42 +0530

    HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,29 @@
-"use client"
+'use client';
+
 import {
   Card,
-  CardHeader,
-  CardBody,
   Typography,
+  IconButton,
+  Carousel,
   Button,
-} from "@material-tailwind/react";
+} from '@material-tailwind/react';
 // import 'animate.css/animate.min.css';
 export default function HorizontalCard() {
   return (
-    <div className="flex flex-wrap mb-4 bg-black w-full">
-      <div className="w-full md:max-w-[900px] flex flex-col md:flex-row mb-5 mt-8 mx-auto md:rounded-lg p-8 bg-gray-200">
-  <div className="order-2 w-full md:w-3/5 md:order-1">
-    <Typography variant="h6" color="black" className="mb-4 text-2xl md:text-4xl font-sans-serif">
+    <div className="flex flex-col mb-4 w-full bg-gradient-to-b from-gray-900 via-gray-800 to-gray-800">
+      <div className="md:max-w-[1600px] md:max-h-[1200] flex flex-col md:flex-row mb-5 mt-8 mx-auto md:rounded-lg p-8 bg-white ">
+  <div className="order-2 w-full md:w-3/5 mb-8 md:mb-0 flex flex-col justify-center">
+    <Typography
+      variant="h6"
+      color="black"
+      className="mb-4 text-4xl md:text-4xl font-sans-serif lg:text-6xl text-center"
+    >
       EVAULT: The future of <span style={{ color: 'red' }}>Legal records</span> keeping
     </Typography>
-    <Typography color="black" className="mb-8 font-sans-serif">
-      Revolutionize legal record keeping - Secure, transparent, accessible for all. Upload, share, track your cases with confidence. The blockchain-powered justice revolution starts here.
+    <Typography color="black" className="mb-8 font-sans-serif text-lg lg:text-xl text-center">
+      Revolutionize legal record keeping - Secure, transparent, accessible for all.
+      Upload, share, track your cases with confidence. The blockchain-powered
+      justice revolution starts here.
     </Typography>
   </div>
   <div className="order-1 w-full md:w-2/5 md:order-2 flex items-center justify-center">
@@ -28,94 +35,50 @@ export default function HorizontalCard() {
   </div>
 </div>
 
-      <Typography variant="h4" color="white" className="font-bold text-xl md:text-2xl lg:text-3xl mb-8 mx-auto font-serif">
-        Design and Architecture of the eVault System
+
+      <Typography
+        variant="h4"
+        color="white"
+        className="font-bold text-xl md:text-2xl lg:text-3xl mb-8 mx-auto font-serif"
+      >
+        Design and Architecture of the EVault System
       </Typography>
-      <div className="flex flex-wrap justify-between ">
-       
-        <div className="w-full md:w-[30%] mb-4 p-4 md:flex-grow animate__animated animate__slideInLeft">
-          <Card>
-            <CardHeader
-              shadow={false}
-              floated={false}
-              className="m-0 w-full h-40 md:h-auto shrink-0 rounded-r-none flex items-center justify-center"
-              style={{ backgroundColor: 'transparent' }}
-            >
-              <img
-                src="/images/img_rectangle16.png"
-                className="h-3/4 w-3/4 object-cover"
-                alt="Blockchain Architecture"
-                style={{ backgroundColor: 'transparent' }}
-              />
-            </CardHeader>
-            <CardBody className="text-center">
-              <Typography variant="h6" color="black" className="mb-4 text-2xl font-sans-serif">
-                Blockchain Architecture
-              </Typography>
-              <Typography color="black" className="mb-8 text-xl font-sans-serif">
-                The eVault system consists of a decentralized network of nodes, each of which stores a copy of all legal records.
-              </Typography>
-            </CardBody>
-          </Card>
+
+      <Carousel  transition={{ duration: 2 }} className="rounded-xl">
+        <div className="w-3/4 md:max-w-[300px] md:max-h-[300px] flex flex-col md:flex-row mb-5 mt-8 mx-auto md:rounded-lg p-8 bg-tranparent relative">
+          <img
+            src="/images/arch.jpg"
+            className="h-full w-full object-cover"
+            alt="Blockchain Architecture"
+            style={{ backgroundColor: 'transparent' }}
+          />
         </div>
 
-       
-        <div className="w-full md:w-[30%] mb-4 p-4 md:flex-grow animate__animated animate__zoomIn">
-          <Card>
-            <CardHeader
-              shadow={false}
-              floated={false}
-              className="m-0 w-full h-40 md:h-auto shrink-0 rounded-r-none flex items-center justify-center"
-              style={{ backgroundColor: 'transparent' }}
-            >
-              <img
-                src="/images/img_rectangle15.png"
-                className="h-3/4 w-full object-cover"
-                alt="Data Storage"
-                style={{ backgroundColor: 'transparent' }}
-              />
-            </CardHeader>
-            <CardBody className="text-center">
-              <Typography variant="h6" color="black" className="mb-4 text-2xl font-sans-serif">
-                Data Storage
-              </Typography>
-              <Typography color="black" className="mb-8 text-xl font-sans-serif">
-                Legal records are encrypted and stored in blocks, which are then added to the blockchain in a chronological chain.
-              </Typography>
-            </CardBody>
-          </Card>
+        <div className="w-3/4 md:max-w-[300px] flex flex-col md:flex-row mb-5 mt-8 mx-auto md:rounded-lg p-8 bg-tranparent relative">
+          <img
+            src="/images/datast.jpg"
+            className="h-full w-full object-cover"
+            alt="Data storage"
+            style={{ backgroundColor: 'transparent' }}
+          />
         </div>
 
-        
-        <div className="w-full md:w-[30%]  mb-4 p-4 md:flex-grow animate__animated animate__slideInRight">
-          <Card>
-            <CardHeader
-              shadow={false}
-              floated={false}
-              className="m-0 w-full h-40 md:h-auto shrink-0 rounded-r-none flex items-center justify-center"
-              style={{ backgroundColor: 'transparent' }}
-            >
-              <img
-                src="/images/img_rectangle17.png"
-                className="h-3/4 w-full object-cover"
-                alt="Access Control"
-                style={{ backgroundColor: 'transparent' }}
-              />
-            </CardHeader>
-            <CardBody className="text-center">
-              <Typography variant="h6" color="black" className="mb-4 text-2xl font-sans-serif">
-                Access Control
-              </Typography>
-              <Typography color="black" className="mb-8 text-xl font-sans-serif">
-                Access to the eVault system is controlled using cryptographic keys, which can be granted or revoked by the network administrator.
-              </Typography>
-            </CardBody>
-          </Card>
+        <div className="w-3/4 md:max-w-[300px] flex flex-col md:flex-row mb-5 mt-8 mx-auto md:rounded-lg p-8 bg-tranparent relative">
+          <img
+            src="/images/access.jpg"
+            className="h-full w-full object-cover"
+            alt="Access control"
+            style={{ backgroundColor: 'transparent' }}
+          />
         </div>
-      </div>
+      </Carousel>
 
       <div className="w-full text-center max-w-[70rem] bg-grey mb-4 mt-8 mx-auto rounded-lg p-8">
-        <Typography variant="h4" color="white" className="font-bold text-3xl mb-8 mx-auto">
+        <Typography
+          variant="h4"
+          color="white"
+          className="font-bold text-3xl mb-8 mx-auto"
+        >
           What can you do with eVault?
         </Typography>
         <div className="flex flex-wrap justify-between">
@@ -124,7 +87,8 @@ export default function HorizontalCard() {
               UPLOAD DOCUMENTS
             </Typography>
             <Typography color="black" className="text-md text-xl font-normal">
-              Seamlessly upload your legal documents to LegalChain eVault, where they will be stored securely and permanently.
+              Seamlessly upload your legal documents to LegalChain eVault, where they will
+              be stored securely and permanently.
             </Typography>
           </Card>
           <Card className="w-full md:w-[30%] bg-teal-500 mb-4 p-4">
@@ -132,7 +96,8 @@ export default function HorizontalCard() {
               SHARE DOCUMENTS
             </Typography>
             <Typography color="black" className="text-md text-xl font-normal">
-              Collaborate effortlessly by sharing legal documents with authorized stakeholders, promoting efficient communication within your legal team.
+              Collaborate effortlessly by sharing legal documents with authorized
+              stakeholders, promoting efficient communication within your legal team.
             </Typography>
           </Card>
           <Card className="w-full md:w-[30%] bg-gray-200 mb-4 p-4">
@@ -140,17 +105,20 @@ export default function HorizontalCard() {
               TRACK HISTORY
             </Typography>
             <Typography color="black" className="text-md text-xl font-normal">
-              Track the complete history of your legal documents on the blockchain, benefiting from a tamper-resistant and auditable record of all transactions and changes.
+              Track the complete history of your legal documents on the blockchain,
+              benefiting from a tamper-resistant and auditable record of all transactions
+              and changes.
             </Typography>
           </Card>
         </div>
       </div>
-      <footer className="flex w-full md:w-10/10 flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t py-6 text-center md:justify-between bg-white text-white shadow-md" >
+      <footer className="flex w-full md:w-10/10 flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t py-6 text-center md:justify-center bg-white text-white shadow-md ">
+      {/* <Chip value="LegalChain" className="mr-10" /> */}
         <Typography color="black" className="font-normal">
           &copy; 2024 LEGAL CHAIN- All Rights Reserved
         </Typography>
-        <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
-          <li>
+        <ul className="flex flex-wrap items-center gap-y-20 gap-x-10">
+          <li >
             <Typography
               as="a"
               href="/about"
